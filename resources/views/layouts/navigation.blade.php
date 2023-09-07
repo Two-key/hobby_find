2e720650d3f9a2f354386eea4ce45c9433ddeb7b
Two-key <21ue006@mb2.fwu.ac.jp>
2023-09-07 09:57:35 +0900

    header部分の編集

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,12 +17,12 @@
                     </x-nav-link>-->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex hover:bg-yellow-400">
                     <x-nav-link :href="route('group_like')" :active="request()->routeIs('group_like')" class=text-yellow-400>
-                        {{ __('お気に入り') }}
+                        {{ __('Favorite') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex hover:bg-yellow-400">
                     <x-nav-link :href="route('leader_create')" :active="request()->routeIs('leader_create')" class=text-yellow-400>
-                        {{ __('マイシップ') }}
+                        {{ __('Leadership') }}
                     </x-nav-link>
                 </div>
             </div>
