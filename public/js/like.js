24925836c8067c437b94a1aad90184625add1820
Two-key <21ue006@mb2.fwu.ac.jp>
2023-09-27 16:50:04 +0900

    like機能の更新

--- public/js/like.js
+++ public/js/like.js
@@ -1,31 +1,31 @@
 $(function () {
-  let like = $('.like-toggle'); //like-toggleのついたiタグを取得し代入。
-  let likeGroupId; //変数を宣言（なんでここで？）
-  like.on('click', function () { //onはイベントハンドラー
-    let $this = $(this); //this=イベントの発火した要素＝iタグを代入
-    likeGroupId = $this.data('group-id'); //iタグに仕込んだdata-review-idの値を取得
-    //ajax処理スタート
+  let like = $('.like-toggle'); 
+  let likeGroupId; 
+  like.on('click', function () {
+    let $this = $(this); 
+    likeGroupId = $this.data('group-id'); 
+   
     $.ajax({
-      headers: { //HTTPヘッダ情報をヘッダ名と値のマップで記述
+      headers: { 
         'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
-      },  //↑name属性がcsrf-tokenのmetaタグのcontent属性の値を取得
-      url: '/like', //通信先アドレスで、このURLをあとでルートで設定します
-      method: 'POST', //HTTPメソッドの種別を指定します。1.9.0以前の場合はtype:を使用。
-      data: { //サーバーに送信するデータ
-        'group_id': likeGroupId //いいねされた投稿のidを送る
+      }, 
+      url: '/like', 
+      method: 'POST', 
+      data: { 
+        'group_id': likeGroupId 
       },
     })
-    //通信成功した時の処理
+    
     .done(function (data) {
-      $this.toggleClass('liked'); //likedクラスのON/OFF切り替え。
+      $this.toggleClass('liked'); 
       $this.next('.like-counter').html(data.group_likes_count);
     })
-    //通信失敗した時の処理
+    
     .fail(function (jqXHR, textStatus, errorThrown) {
       console.log('fail');
-      console.log("jqXHR          : " + jqXHR.status); // HTTPステータスが取得
-                    console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラー
-                    console.log("errorThrown    : " + errorThrown.message); // 例外情報
+      console.log("jqXHR          : " + jqXHR.status); 
+                    console.log("textStatus     : " + textStatus);
+                    console.log("errorThrown    : " + errorThrown.message);
     });
   });
   });
